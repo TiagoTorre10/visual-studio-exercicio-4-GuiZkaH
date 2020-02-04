98c0ee2099538065fbc891d3f7c4cddd45b44f0e
GuiZkaH <52414162+GuiZkaH@users.noreply.github.com>
2020-02-04 14:58:00 +0000

    Project

---
+++ Projeto_ficha1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1;
+	int num2;
+
+	cout << "Insira um numero: ";
+	cin >> num1;
+	cout << "Insira outro numero: ";
+	cin >> num2;
+	cout << "A soma e " << num1 + num2 << ".";
+}
